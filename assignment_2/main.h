4231269977abcf4dd34b8214c46357bba0ccf5e9
Luan Lima <luanbrl@live.com>
2020-06-29 19:31:50 -0300

    workaround for compile on macOS

--- assignment_2/main.h
+++ assignment_2/main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+// Workaround for compile on macOS
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#else
 #include <GL/glut.h>
+#include <GL/gl.h>
+#endif
+
 #include <stdio.h>
+#include <stdlib.h>
 
 #endif  // MAIN_H
