8669fe09cf04699c5a350f93daa97d7b5041c535
Luan Lima <luanbrl@live.com>
2020-08-09 21:15:11 -0300

    exercise 2 implemented

--- assignment_3/main.cpp
+++ assignment_3/main.cpp
@@ -66,10 +66,10 @@ void Display(void) {
 
     // Matriz Model ///////////////////////////////////////////////////////////
     // You will have to change the contents of this matrix for the exercises
-    float model_array[16] = {1.0f*(0.33), 0.0f, 0.0f, 0.0f, 
-                             0.0f, 1.0f*(1.50), 0.0f, 0.0f, 
+    float model_array[16] = {1.0f, 0.0f, 0.0f, 0.0f, 
+                             0.0f, 1.0f, 0.0f, 0.0f, 
                              0.0f, 0.0f, 1.0f, 0.0f, 
-                             0.0f, 0.0f, 0.0f, 1.0f};
+                             1.0f, 0.0f, 0.0f, 1.0f};
     glm::mat4 model_mat = glm::make_mat4(model_array);
 
     // Matriz View ////////////////////////////////////////////////////////////
